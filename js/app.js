93711fa692ed955cae7b4f7b15447e242873d4c0
Hofman <Mateusz.Hofman@BAIN.com>
2023-04-05 23:48:38 +0200

    Before fixing Intervals multiplication --> Only add one fresh Interval when clicked

--- js/app.js
+++ js/app.js
@@ -1,24 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const blurredImages = document.querySelectorAll(".about-us__blurred-image");
+  //////////////////////////////////////////////////////////////////////
+  // Set intervals and moving algoritm
+  /////////////////////////////////////////////////////////////////////
 
-  // Below will work smoothly if setInterval works every ${randomDelay *2}s;
+  // movingBlurredImages() will work smoothly if setInterval works every ${randomDelay *2}s;
   // Because of Transition == randomDelay && TransitionDelay == randomDelay;
   // So that way it doesn't cut animation in half
 
-  function movingBluuredImages() {}
+  function movingBlurredImages() {
+    // Dirty way (!!) - TBH not even working now
+    // How to get inside the stack of moveIntervals ?????
+    // They were declared inside a function but when this function ends,
+    // there might not be the way to get back there
+    for (i = 0; i < 40; i++) {
+      window.clearInterval(i);
+      console.log("działa for");
+    }
 
-  blurredImages.forEach(function (element) {
-    const randomDelay = Math.random() * 3;
-    element.style.transition = `${randomDelay} cubic-bezier(0.62, 0.32, 0, 0.9)`;
-    element.style.transitionDelay = `${randomDelay}s`;
-    element.style.top = generateYPosJs(100);
+    const blurredImages = document.querySelectorAll(".about-us__blurred-image");
 
-    const interval = setInterval(function () {
-      blurredImages.forEach(function (element) {
+    blurredImages.forEach(function (element) {
+      const randomDelay = Math.random() * 3;
+      element.style.transition = `${randomDelay} cubic-bezier(0.62, 0.32, 0, 0.9)`;
+      element.style.transitionDelay = `${randomDelay}s`;
+      element.style.top = generateYPosJs(100);
+
+      const moveInterval = setInterval(function () {
         element.style.top = generateYPosJs(100);
-      });
-    }, 6000);
-  });
+        console.log("odpalam interval");
+      }, 6000);
+    });
+  }
 
   function generateYPosJs(spread) {
     const randomNumber = Math.random() * 100;
@@ -33,57 +45,71 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // movingBluuredImages();
+  movingBlurredImages();
 
+  //////////////////////////////////////////////////////////////////////
+  // NAVI - Currently not working
   /////////////////////////////////////////////////////////////////////
 
-  // NAV
   const naviContainer = document.querySelector(".main-comment__navi");
   const prevButton = naviContainer.querySelector(".navi__button--prev");
   const nextButton = naviContainer.querySelector(".navi__button--next");
 
   prevButton.addEventListener("click", function (e) {
     console.log("This is a prev button");
-    console.log(e);
   });
 
   nextButton.addEventListener("click", function () {
     console.log("This is a next button");
   });
 
+  //////////////////////////////////////////////////////////////////////
+  // Enable clicking on headshots
   /////////////////////////////////////////////////////////////////////
 
-  const allHeadshots = document.querySelectorAll(".headshot");
-  // console.log(allHeadshots);
+  let allHeadshots;
+
+  const getHeadshots = function () {
+    allHeadshots = document.querySelectorAll(".headshot");
+  };
+
+  getHeadshots();
+
   allHeadshots.forEach(function (element, i) {
-    element.addEventListener("click", function (e) {
-      element.style.filter = "blur(0px)";
-      element.style.transition = `1s cubic-bezier(0.79, 0.01, 0.29, 1.01)`;
-      // const mainClassList = "main-comment__image, headshot";
-
-      const allCommentsContainer = document.querySelector(
-        ".about-us__comment-avatars"
-      );
-      const currentMainParent = allCommentsContainer.querySelector(
-        ".main-comment__image-container"
-      );
-      const currentMain = currentMainParent.querySelector(
-        ".main-comment__image"
-      );
-
-      currentMain.style.filter = "blur(8px)";
-      currentMain.style.transition = "2s";
-
-      currentMainParent.style.height = "433.99px";
-
-      currentMain.classList.remove(`main-comment__image`);
-      allCommentsContainer.insertBefore(currentMain, allHeadshots[i]);
-
-      currentMain.classList.add(
-        `about-us__blurred-image`,
-        `headshot`,
-        `blurred-image--${i + 1}`
-      );
-    });
+    element.addEventListener("click", clickBluured);
   });
+
+  //////////////////////////////////////////////////////////////////////
+  // Click function declaration
+  /////////////////////////////////////////////////////////////////////
+
+  function clickBluured(e) {
+    getHeadshots();
+    e.currentTarget.style.filter = "blur(0px)";
+    // e.currentTarget.style.transition = `1s cubic-bezier(0.79, 0.01, 0.29, 1.01)`;
+    // const mainClassList = "main-comment__image, headshot";
+
+    const allCommentsContainer = document.querySelector(
+      ".about-us__comment-avatars"
+    );
+    const currentMainParent = allCommentsContainer.querySelector(
+      ".main-comment__image-container"
+    );
+    const currentMain = currentMainParent.querySelector(".main-comment__image");
+
+    currentMain.style.filter = "blur(8px)";
+    currentMainParent.style.height = "433.99px";
+
+    currentMain.classList.remove(`main-comment__image`);
+    allCommentsContainer.insertBefore(currentMain, e.currentTarget);
+
+    //////////////////////////
+    currentMainParent.appendChild(e.currentTarget);
+    currentMain.classList = e.currentTarget.classList;
+    e.currentTarget.classList = "";
+    e.currentTarget.classList.add(`main-comment__image`);
+
+    e.currentTarget.removeEventListener("click", clickBluured);
+    movingBlurredImages();
+  }
 });
